f49a1ef9e6aeb472ad11d3960c4d431da14b972f
Dico Didiraja <didiraja@hotmail.com>
2017-03-16 22:23:57 -0300

    ajustes finos; arquivos otimizados;

--- site/js/main.js
+++ site/js/main.js
@@ -61,16 +61,6 @@ function main() {
         offset: 80
     })
 
-  	$(document).ready(function() {
-  	    $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
-
-  	});
-
   	// Portfolio Isotope Filter
     $(window).load(function() {
         var $container = $('.portfolio-items');
@@ -111,10 +101,6 @@ function main() {
   }
   initParallax();
 
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
 
 }());
 
